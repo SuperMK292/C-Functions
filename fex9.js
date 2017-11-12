d30cca09a17f58d77e680ff29f67e51f4ca6314c
SuperMK292 <33517890+SuperMK292@users.noreply.github.com>
2017-11-12 12:15:59 +0200

    Update fex9.js

--- fex9.js
+++ fex9.js
@@ -1,9 +1,9 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 
 void plusMinus(int num1, int num2)
 {
     int i, dbl, big, small;
-    
+
 if(num2 > num1)
 {
     big = num2;
@@ -15,15 +15,15 @@ else
     small = num2;
 }
 
-for(i = small; i >= big; i++)
+for(i = small; i <= big; i++)
 {
-    if(i % 2 == 0)
+    if(i % 2 == 1)
         printf("%d\n", i);
     else
     {
         dbl = i * 2;
         dbl = i - dbl;
-        printf("%d\n", dbl); 
+        printf("%d\n", dbl);
     }
 }
 }
@@ -32,7 +32,7 @@ int main()
     int num1, num2;
 
     printf("please enter a number:\n");
-    scanf("%d %d", num1, num2);
+    scanf("%d %d", &num1, &num2);
     plusMinus(num1, num2);
 
     return 0;
